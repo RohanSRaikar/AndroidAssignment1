b54f40bdd2bd4f74a973aa9b926696a1e8a54123
Rohan.Raikar <rohanraikar@live.com>
2017-03-23 11:32:09 +0530

    Completed Task 1. Fetching Current location 2. Setting camera to our current location 3. Getting address of our current location 4. Showing route from current location to the selected location 5. Calculating the distance of the selected location

--- app/src/main/java/example/rohanraikar/com/androidassignment/MainActivity.java
+++ app/src/main/java/example/rohanraikar/com/androidassignment/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -36,13 +37,18 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     GoogleMap map;
     ArrayList<LatLng> markerPoints;
-    TextView tvDistanceDuration;
-
+    TextView tvDistanceDuration,tvFrom;
+    TempStorage myStore;
+    LatLng latLng;
+    double curLat,curlon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myStore=new TempStorage(getApplicationContext());
         tvDistanceDuration = (TextView) findViewById(R.id.TV_distanceTime);
+        tvFrom=(TextView)findViewById(R.id.TV_fromLocation);
+        tvFrom.setText("From : "+myStore.getValueFromMystore("LocationData","myAddress"));
         // Initializing
         markerPoints = new ArrayList<LatLng>();
         // Getting reference to SupportMapFragment of the activity_main
@@ -51,7 +57,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         // Enable MyLocation Button in the Map
         // Setting onclick event listener for the map
-
+        curLat=Double.parseDouble(myStore.getValueFromMystore("LocationData","latitude"));
+        curlon=Double.parseDouble(myStore.getValueFromMystore("LocationData","longitude"));
+        latLng=new LatLng(curLat,curlon);
     }
 
     private String getDirectionsUrl(LatLng origin, LatLng dest) {
@@ -60,7 +68,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         // Destination of route
         String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
         // Sensor enabled
-        String sensor = "sensor=false";
+        String sensor = "sensor=true";
         // Building the parameters to the web service
         String parameters = str_origin + "&" + str_dest + "&" + sensor;
         // Output format
@@ -104,6 +112,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(curLat, curlon), 15));
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
         setUpMap();
     }
 
@@ -129,7 +139,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onMapClick(LatLng point) {
                 // Already two location
-                if (markerPoints.size() > 1) {
+                if (markerPoints.size() > 0) {
                     markerPoints.clear();
                     map.clear();
                 }
@@ -143,22 +153,25 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                  * For the start location, the color of marker is GREEN and
                  * for the end location, the color of marker is RED.
                  */
-                if (markerPoints.size() == 1) {
+                if (markerPoints.size() == 0) {
                     options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-                } else if (markerPoints.size() == 2) {
+                }/* else if (markerPoints.size() == 2) {
                     options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                }
+                }*/
                 // Add new marker to the Google Map Android API V2
                 map.addMarker(options);
                 // Checks, whether start and end locations are captured
-                if (markerPoints.size() >= 2) {
-                    LatLng origin = markerPoints.get(0);
-                    LatLng dest = markerPoints.get(1);
+                if (markerPoints.size() >= 0) {
+                    LatLng origin = latLng;
+                    LatLng dest = markerPoints.get(0);
                     // Getting URL to the Google Directions API
                     String url = getDirectionsUrl(origin, dest);
                     DownloadTask downloadTask = new DownloadTask();
                     // Start downloading json data from Google Directions API
                     downloadTask.execute(url);
+                    DownloadTask downloadTask1 = new DownloadTask();
+                    // Start downloading json data from Google Directions API
+                    downloadTask1.execute(url+"&mode=walking");
                 }
             }
         });
@@ -174,6 +187,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             try{
                 // Fetching the data from web service
                 data = downloadUrl(url[0]);
+                Log.d("Rohan","Received data:"+data);
                 }catch(Exception e){
                 Log.d("Background Task",e.toString());
                 }
@@ -216,7 +230,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             MarkerOptions markerOptions = new MarkerOptions();
             String distance = "";
             String duration = "";
-            if(result.size()<1){
+            if(result.size()<0){
                 Toast.makeText(getBaseContext(), "No Points", Toast.LENGTH_SHORT).show();
                 return;
               }
@@ -245,8 +259,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 }
                 // Adding all the points in the route to LineOptions
                 lineOptions.addAll(points);
-                lineOptions.width(2);
-                lineOptions.color(Color.RED);
+                lineOptions.width(10);
+                lineOptions.color(Color.BLUE);
              }
              tvDistanceDuration.setText("Distance:"+distance + ", Duration:"+duration);
              // Drawing polyline in the Google Map for the i-th route
